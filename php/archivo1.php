7da0bc788059c29700d0aa002da6341a0599a418
AlfredoAlvarez3 <alfredoalvarez10056@gmail.com>
2022-07-08 15:04:44 -0500

    07/07/22

---
+++ php/archivo1.php
@@ -0,0 +1,6 @@
+<?php
+
+echo "<h1>Alfredo Alvarez Lopez </h1>";
+echo "tercer cuatrimestre grupo A";
+
+?>
---
+++ php/archivo2.php
@@ -0,0 +1,21 @@
+<?php
+$var1=8;
+$var2=7;
+$var3=$var1+$var2;
+echo $var3;
+
+echo "<hr>";
+$var4=number_format(4.3,2 );
+$var5=number_format(4.3,2 );
+echo$var4+$var5;
+echo "<hr>";
+
+$var6=20;
+$var7=100;
+
+$multi = $var6 * $var7;
+
+echo $var6." X ".$var7."=".$multi. "<br>";
+//para concatenar o unir texto en PHP utilizamos el operador.
+
+?>
